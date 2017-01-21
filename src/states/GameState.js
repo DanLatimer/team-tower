9f0f517d1942dd98f9c18c3b70571cc5c3f796c9
Daniel Latimer <daniel.latimer@trackgrp.com>
2017-01-20 21:58:52 -0400

    cleanup

--- src/states/GameState.js
+++ src/states/GameState.js
@@ -4,22 +4,15 @@ import {Resources} from 'resources';
 
 class GameState extends Phaser.State {
 	preload() {
-		debugger;
 		// IMAGES
 		Resources.images
 			.forEach(image => this.game.load.image(image.name, image.path));
-
 	}
 
 	create() {
-		debugger;
 		let center = { x: this.game.world.centerX, y: this.game.world.centerY }
-		//let text = new RainbowText(this.game, center.x, center.y, "- phaser -\nwith a sprinkle of\nES6 dust!");
-
 		var turret = new BasicTurret(this.game, {x: 200, y: 200});
-		//text.anchor.set(0.5);
 	}
-
 }
 
 export default GameState;

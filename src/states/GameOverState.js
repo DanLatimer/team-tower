cf75a0cd1ecb95d6ef5ddb35762a1d9538694a88
Nathan Myles <myles.n.a@gmail.com>
2017-01-22 20:09:04 -0400

    Reload after finish

--- src/states/GameOverState.js
+++ src/states/GameOverState.js
@@ -5,13 +5,19 @@ class GameOverState extends Phaser.State {
     }
 
     create() {
-        let center = { x: this.game.world.centerX, y: this.game.world.centerY };
+        let center = {x: this.game.world.centerX, y: this.game.world.centerY};
 
         const gameOverStateSprite = this.win ? 'youWin' : 'youLose';
 
-        var s = this.add.sprite(center.x, center.y, gameOverStateSprite);
+        let s = this.add.sprite(center.x, center.y, gameOverStateSprite);
         s.anchor.setTo(0.5, 0.5);
         this.stage.backgroundColor = "#383838";
+
+        const bitmapText = this.add.bitmapText(center.x - 50, center.y + 100, 'desyrel', 'Reload');
+        bitmapText.inputEnabled = true;
+        bitmapText.events.onInputDown.add(() => {
+            window.location.reload();
+        })
     }
 }
 

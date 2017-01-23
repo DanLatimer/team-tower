dd4673e86010cf7d350f789bc471da18e0c51d89
Nathan Myles <myles.n.a@gmail.com>
2017-01-22 20:09:22 -0400

    remove input manager

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import GridManager from 'managers/GridManager';
-import InputManager from 'managers/InputManager';
 import InventoryManager from 'managers/InventoryManager';
 import GUIManager from 'managers/GUIManager';
 import WaveManager from 'managers/WaveManager';
@@ -11,7 +10,6 @@ class Game extends Phaser.Game {
 		super(800, 600, Phaser.AUTO, 'content', null);
 
         this.gridManager = new GridManager(this);
-        this.inputManager = new InputManager(this);
         this.inventoryManager = new InventoryManager(this);
         this.guiManager = new GUIManager(this);
         this.waveManager = new WaveManager(this);

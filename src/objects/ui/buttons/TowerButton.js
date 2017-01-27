1bfb849a3b82d14e99fbd02119b014d8bc2b4c0e
Daniel Latimer <daniellatimer@gmail.com>
2017-01-27 19:46:41 -0400

    make towers config based

--- src/objects/ui/buttons/TowerButton.js
+++ src/objects/ui/buttons/TowerButton.js
@@ -1,21 +1,21 @@
 import Button from './Button';
 
 class TowerButtonButton extends Button {
-    constructor(game, position, towerClass) {
-        super(game, position, towerClass.sprite);
+    constructor(game, position, inventoryItem) {
+        super(game, position, inventoryItem.sprite);
 
-        this.towerClass = towerClass;
+        this.inventoryItem = inventoryItem;
         this.selected = false;
         this.alpha = 0.5;
     }
 
     update() {
-        var isSelected = this.game.inventoryManager.getSelectedTowerClass() === this.towerClass;
+        var isSelected = this.game.inventoryManager.getSelectedInventoryItem() === this.inventoryItem;
         this.alpha = isSelected ? 1 : 0.5; 
     }
 
     onClick() {
-        this.game.inventoryManager.selectTower(this.towerClass);
+        this.game.inventoryManager.selectTower(this.inventoryItem);
     }
 }
 
